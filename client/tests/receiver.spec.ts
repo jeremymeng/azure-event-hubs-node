05bf084235e20408c551e5c90108718b1e9e0aba
Jeremy Meng <yumeng@microsoft.com>
2019-02-05 11:24:07 -0800

    Remove mocha import to get tests running

--- client/tests/receiver.spec.ts
+++ client/tests/receiver.spec.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import "mocha";
 import chai from "chai";
 import uuid from "uuid/v4";
 const should = chai.should();
